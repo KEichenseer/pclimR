0c624fcf4a1abb821616b4630a378fe9bca9db04
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-03-03 14:42:52 +0100

    update to lat_bins function
    latitudinal bin function updated to include fit parameter

--- R/lat_bins.R
+++ R/lat_bins.R
@@ -3,13 +3,24 @@
 #' A function to generate latitudinal bins for a given bin size.
 #'
 #' @param size A single numeric value of > 0 and <= 90.
+#' @param fit A logical value indicating whether a bin size check should be performed to ensure that the
+#' entire latitudinal range is covered (-90 to 90).
 #' @return A \code{data.frame} of latitudinal bins for a given size.
 #' @examples
-#' lat_bins(size = 10)
-#' lat_bins(size = 30)
+#' lat_bins(size = 20)
+#' lat_bins(size = 13, fit = TRUE)
 
-lat_bins <- function(size = 10){
+lat_bins <- function(size = 10, fit = FALSE){
   bins <- 180/size
+  if(fit == TRUE){
+    if(is.integer(bins) == FALSE){
+      int <- 180/seq(from = 1, to = 90, by = 1)
+      int <- which(int%%1==0)
+      size <- int[which.min(abs(int - size))]
+      bins <- 180/size
+      message(paste0("Bin size set to ",size," degrees to cover latitudinal range."))
+    }
+  }
   df <- seq(from = -90, to = 90, by = size)
   min <- df[1:bins]
   max <- df[1:bins]+size
